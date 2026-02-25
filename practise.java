6f0d246c728638e8b43ca76989084ae54396536f
Sarim <151559963+Syed-M-Sarim@users.noreply.github.com>
2026-02-26 02:09:51 +0500

    ternary operator

---
+++ practise.java
@@ -0,0 +1,7 @@
+public class practise {
+    public static void main (String [] args){
+        int marks = 49;
+        String reportCard = (marks >= 50) ? "Pass" : "Fail";
+        System.out.println(reportCard);
+    }
+}
