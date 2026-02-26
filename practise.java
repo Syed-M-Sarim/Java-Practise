e47dd39b7e881f7c027b72bf93afa5d341fea7a7
Sarim <151559963+Syed-M-Sarim@users.noreply.github.com>
2026-02-27 01:03:29 +0500

    While loop practise

--- practise.java
+++ practise.java
@@ -1,24 +1,9 @@
 public class practise {
     public static void main (String [] args){
-        int days = 9;
-        switch (days) {
-            case 1: System.out.println("Monday");
-                break;
-            case 2: System.out.println("Tuesday");
-                break;
-            case 3: System.out.println("Wednesday");
-                break;
-            case 4: System.out.println("Thursday");
-                break;
-            case 5: System.out.println("Friday");
-                break;
-            case 6: System.out.println("Saturday");
-                break;
-            case 7: System.out.println("Sunday");
-                break;
-        
-            default: System.out.println("Invalid input");
-                break;
-        }
+      int count = 1;
+      while(count<11){
+        System.out.println(count);
+        count+=1;
+      }
     }
 }
