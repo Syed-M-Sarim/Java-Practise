d9f1c2970950e28754507139a5bceb4df95a0e4f
Sarim <151559963+Syed-M-Sarim@users.noreply.github.com>
2026-02-27 15:53:07 +0500

    practise

--- practise.java
+++ practise.java
@@ -1,11 +1,16 @@
 import java.util.*;
 public class practise {
     public static void main (String [] args){
-      int i = 1;
-      do{
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter range to stop at: ");
+        int range = sc.nextInt();
+      for (int i=1; i<=range; i++){
+        if (i == 10){
+            System.out.println(i +" "+"Break point");
+            break;
+        }
         System.out.println(i);
-        i++;
       }
-      while(i<=5);
+      sc.close();
     }
 }
