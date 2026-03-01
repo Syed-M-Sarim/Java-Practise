f1f27ee3ae2f1f63ab9f2e89276093b75aa8fca6
Sarim <151559963+Syed-M-Sarim@users.noreply.github.com>
2026-03-02 02:22:59 +0500

    Numbers pyramid pattern

--- practise.java
+++ practise.java
@@ -1,9 +1,9 @@
 import java.util.*;
 public class practise {
     public static void main (String [] args){
-      for (int i=5; i>=1; i--){
+      for (int i=1; i<=4; i++){
         for (int j=1; j<=i; j++){
-          System.out.print("*");
+          System.out.print(j);
         }
         System.out.println();
       }
