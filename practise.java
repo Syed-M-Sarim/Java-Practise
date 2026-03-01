7b19f19f181052a42436b458a498bc0039b5a40d
Sarim <151559963+Syed-M-Sarim@users.noreply.github.com>
2026-03-02 01:46:26 +0500

    star pattern

--- practise.java
+++ practise.java
@@ -1,16 +1,11 @@
 import java.util.*;
 public class practise {
     public static void main (String [] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter range to stop at: ");
-        int range = sc.nextInt();
-      for (int i=1; i<=range; i++){
-        if (range%i ==0){
-            System.out.println("Break point");
-            continue;
+      for (int i = 1; i<=4; i++) {
+        for (int j = 1; j<=i; j++) {
+          System.out.print("*");
         }
-        System.out.println(i);
+        System.out.println();
       }
-      sc.close();
     }
 }
