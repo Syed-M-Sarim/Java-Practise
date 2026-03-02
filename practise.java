30534b642a32e47c4c055f67aa3102b0cc92e45d
Sarim <151559963+Syed-M-Sarim@users.noreply.github.com>
2026-03-02 18:07:58 +0500

    Intro to Functions

--- practise.java
+++ practise.java
@@ -1,13 +1,14 @@
 import java.util.*;
+
 public class practise {
-    public static void main (String [] args){
-      char ch = 'A';
-      for (int i=1; i<=4; i++){
-        for (int j=1; j<=i; j++){
-          System.out.print(ch);
-          ch++;
-        }
-        System.out.println();
+
+    public static int helloWorld(int a, int b) {
+        int sum = a + b;
+        return sum;
       }
+
+    public static void main(String[] args) {
+        int show = helloWorld(10,10);
+        System.out.print("Sum is: " + show);
     }
 }
