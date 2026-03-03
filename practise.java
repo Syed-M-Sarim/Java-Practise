51a513dc1bf3a3a6c6f3893baf9da608ac108496
Sarim <151559963+Syed-M-Sarim@users.noreply.github.com>
2026-03-04 03:08:15 +0500

    product of 2 numbers

--- practise.java
+++ practise.java
@@ -6,13 +6,13 @@ public class practise {
         System.out.println("Welcome! " + name);
       }
 
-    public static int square(int number){
-        return number*number;
+    public static int square(int number1, int number2){
+        return number1*number2;
     }
 
     public static void main(String[] args) {
         helloWorld("Sarim");
-        int result = square(10);
+        int result = square(10,5);
         System.out.println("Square is: " + result);
     }
 }
