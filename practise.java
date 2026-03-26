e10124f66876713cf18435931bebca8b6ed28ae4
Sarim <151559963+Syed-M-Sarim@users.noreply.github.com>
2026-03-26 20:07:23 +0500

    Patterns practise

--- practise.java
+++ practise.java
@@ -2,16 +2,20 @@ import java.util.*;
 
 public class practise {
 
-    public static int factorial(int n){
-        int f=1;
-        for (int i=1; i<=n; i++){
-            f = f*i;
+    public static void hollow(int totRow, int totCol) {
+        for (int i = 1; i <= totRow; i++) {
+            for (int j = 1; j <= totCol; j++) {
+                if (i == 1 || i == totRow || j == 1 || j == totCol) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
         }
-        return f;
     }
 
     public static void main(String[] args) {
-       int result = factorial(4);
-       System.out.println(result);
+        hollow(4, 5);
     }
 }
