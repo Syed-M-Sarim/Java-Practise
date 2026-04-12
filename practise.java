c671410e80704ca07deb6d6bc0b3db0d2da0592e
Sarim <151559963+Syed-M-Sarim@users.noreply.github.com>
2026-04-12 19:24:48 +0500

    more

--- practise.java
+++ practise.java
@@ -1,13 +1,20 @@
 import java.util.*;
 
 public class practise {
-public static void main(String[] args) {
-    int n = 6;
-    int total = 0;
-    for (int i = 1; i <= n; i++){
-        total = total + i;
-    }
-    System.out.println(total);
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                sum = sum + i;
+            }
+        }
+
+        System.out.println("Sum of even numbers: " + sum);
     }
+}
